359631840b35ad28429bf7d25ad133f2676b3a9a
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-03-14 16:37:49 +0530

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -3,10 +3,45 @@ import os
 from helper import (
     make_issue,
     prepare_result,
-    publish_results,
-    get_files,
+    publish_results
 )
 
-codepath = os.environ.get("CODE_PATH", "/code")
-
 # Add your analyzer logic below
+def analyze():
+    blacklisted_names = ("deepsource", "Deepsource", "deepSource")
+
+    codepath = os.environ.get("CODE_PATH", "/code")
+
+    issues = []
+    lines = []
+
+    filepath = os.path.join(codepath, "README.md")
+    try:
+
+        with open(
+            filepath
+        ) as fp:
+            lines = fp.readlines()
+    except:
+        pass
+
+    for idx, line in enumerate(lines):
+        lno = idx + 1
+        for bad_ds in blacklisted_names:
+            if bad_ds in line:
+                issues.append(
+                    make_issue(
+                        "WATER-001",
+                        f"Blacklisted word: {bad_ds}",
+                        filepath,
+                        lno,
+                        0
+                    )
+                )
+
+    results = prepare_result(issues)
+    publish_results(results)
+
+
+if __name__ == "__main__":
+    analyze()
